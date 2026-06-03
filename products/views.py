b8e110efcc0a175fd37add8741d062a39433176a
jansabith <jansabithjans@gmail.com>
2026-06-03 18:59:06 +0530

    ADDED PRODUCT VIEWS

--- products/views.py
+++ products/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 
-from rest_framework import ListAPIView
+from rest_framework.generics import ListAPIView,RetrieveAPIView
 from .models import Product
 from .serializers import ProductSerializer  
 # Create your views here.
+class ProductListAPIView(ListAPIView):
+
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+
+        queryset = Product.objects.all()
+
+        company = self.request.GET.get("company")
+
+        category = self.request.GET.get("category")
+
+        if company:
+            queryset = queryset.filter(
+                company_id=company
+            )
+
+        if category:
+            queryset = queryset.filter(
+                category_id=category
+            )
+
+        return queryset
+    
+class ProductDetailAPIView(RetrieveAPIView):
+
+    queryset = Product.objects.all()
+
+    serializer_class = ProductSerializer
